01c9179020d631abeeb271ffae8d37910625bc7b
sbatton <sbatton@its.jnj.com>
2020-05-20 15:33:10 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/jobs'
+  get 'static_pages/living'
+  get 'static_pages/fun'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
